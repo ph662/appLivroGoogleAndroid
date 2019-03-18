0d5cfe1c617e3cf3c29119737bd065a295667992
Phelipe de Carvalho Lima <phelipe662@gmail.com>
2019-03-17 22:41:29 -0300

    Usando spinner e spinner adapter.

---
+++ app/src/main/java/br/com/boaviagem/GastoActivity.java
@@ -0,0 +1,67 @@
+package br.com.boaviagem;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import java.util.Calendar;
+
+public class GastoActivity extends Activity {
+
+    private int ano, mes, dia;
+    private Button dataGasto;
+    private Spinner categoria;
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            ano = year;
+            mes = monthOfYear;
+            dia = dayOfMonth;
+            dataGasto.setText(dia + "/" + (mes + 1) + "/" + ano);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gasto);
+
+        buscaDataAtualParaColocarNoBotao();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.categoria_gasto,
+                android.R.layout.simple_spinner_item);
+
+        categoria = findViewById(R.id.categoria);
+        categoria.setAdapter(adapter);
+
+    }
+
+    private void buscaDataAtualParaColocarNoBotao() {
+        Calendar calendar = Calendar.getInstance();
+        ano = calendar.get(Calendar.YEAR);
+        mes = calendar.get(Calendar.MONTH);
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+        dataGasto = findViewById(R.id.data);
+        dataGasto.setText(dia + "/" + (mes + 1) + "/" + ano);
+    }
+
+    public void selecionarData(View view) {
+        showDialog(view.getId());
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (R.id.data == id) {
+            return new DatePickerDialog(this, listener, ano, mes, dia);
+        }
+        return null;
+    }
+}
